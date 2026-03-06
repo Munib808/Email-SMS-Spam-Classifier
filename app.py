37eefe1c43ab2251f3bdd195d84027b525840bc0
Munib808 <www.munibahmad.808@gmail.com>
2026-03-06 23:40:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,54 @@
 import streamlit as st
 import pickle
 import string
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 
-ps = PorterStemmer()
+# Load your TF-IDF vectorizer and model
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
+# Simple stemmer (Porter Stemmer alternative using suffix stripping)
+def simple_stem(word):
+    suffixes = ['ing', 'ly', 'ed', 'ious', 'ies', 'ive', 'es', 's', 'ment']
+    for suffix in suffixes:
+        if word.endswith(suffix) and len(word) > len(suffix) + 2:
+            return word[:-len(suffix)]
+    return word
+
+# Minimal stopwords list (common English words)
+STOPWORDS = set([
+    'a','an','the','is','it','this','that','in','on','for','to','of','and','or','if','are','as','at','be','by','from','has','he','she','i','you','we','they','them','with','was','but'
+])
 
 def transform_text(text):
+    # Lowercase
     text = text.lower()
-    text = nltk.word_tokenize(text)
+    # Remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # Tokenize by whitespace
+    words = text.split()
+    # Remove stopwords
+    words = [word for word in words if word not in STOPWORDS]
+    # Apply simple stemming
+    words = [simple_stem(word) for word in words]
+    return ' '.join(words)
 
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-
-    return " ".join(y)
-
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
-
+# Streamlit UI
 st.title("Email/SMS Spam Classifier")
 
-input_sms = st.text_area("Enter the message")
+input_sms = st.text_area("Enter your message here:")
 
 if st.button('Predict'):
-
-    # 1. preprocess
-    transformed_sms = transform_text(input_sms)
-    # 2. vectorize
-    vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
-    result = model.predict(vector_input)[0]
-    # 4. Display
-    if result == 1:
-        st.header("Spam")
+    if input_sms.strip() == "":
+        st.warning("Please enter a message to classify!")
     else:
-        st.header("Not Spam")
+        # Preprocess
+        transformed_sms = transform_text(input_sms)
+        # Vectorize
+        vector_input = tfidf.transform([transformed_sms])
+        # Predict
+        result = model.predict(vector_input)[0]
+        # Display result
+        if result == 1:
+            st.header("Spam 🚫")
+        else:
+            st.header("Not Spam ✅")
